0eed3aa9724869c71d210b105df7c5a7ef1fbc9f
RickFord117 <rhenriques@spartaglobal.com>
2018-07-30 17:43:12 +0100

    Python install unit test passed

--- Berksfile
+++ Berksfile
@@ -2,4 +2,4 @@ source 'https://supermarket.chef.io'
 
 cookbook 'poise-python', '~> 1.7.0'
 
-metadate
+metadata
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,16 +20,19 @@ describe 'python::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::ServerRunner.new(platform: 'Ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+    it 'Should install python' do
+      expect( chef_run ).to install_package 'python'
     end
   end
 end
